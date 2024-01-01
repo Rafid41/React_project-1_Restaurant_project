b6806112c7273d234bbc64a633ed67787cf36b59
Rafid41 <wap.alnahiyan525@gmail.com>
2024-01-02 03:27:39 +0600

    Redux: create Reducer | [file: src\redux\reducer.js]

---
+++ src/redux/reducer.js
@@ -0,0 +1,14 @@
+//src\redux\reducer.js
+
+import DISHES from "../data/dishes";
+import COMMENTS from "../data/comments";
+
+// main task: DISHES, COMMENTS k globally accessible kora
+const initialState = {
+    dishes: DISHES,
+    comments: COMMENTS
+}
+
+export const Reducer = (state = initialState, action) => {
+    return state;
+}
